36a2da5239971c08c1384de5bfcb08652c699f96
indra0410 <115885675+indra0410@users.noreply.github.com>
2025-10-04 19:37:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // const fs = require("fs");
-// // fs.writeFileSync('read.txt',"hello i am indranil");
-// // fs.appendFileSync('read.txt'," i am a software developer");
-// // fs.renameSync("read.txt","newRead.txt");
+// fs.writeFileSync('read.txt',"hello i am indranil");
+// fs.appendFileSync('read.txt'," i am a software developer");
+// fs.renameSync("read.txt","newRead.txt");
 
 // const data  = fs.readFileSync('newRead.txt');
 // console.log(data);
 // const org_data = data.toString();
 // console.log(org_data);
 //---------------------------------------------------------------
-//os module
 // const os = require("os");
 // console.log(os.hostname());
 ///-----------------------------------------
