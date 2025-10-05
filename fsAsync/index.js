92fdd771084813fc55044ec39a620375d6bdf363
indra4212001 <116474933+indra4212001@users.noreply.github.com>
2025-10-05 09:30:09 +0530

    Update index.js

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -1,11 +1,11 @@
-// const fs = require("fs");
+const fs = require("fs");
 
 // // fs.writeFile("read.txt","i am indranil",(eror)=>{
 // //     console.log('file created');
 // //     console.log(eror)
 // // })
 
-// fs.readFile('read.txt','utf8',(eror,data) => {
-//   console.log(data);
-//   console.log(eror)
-// })
+fs.readFile('read.txt','utf8',(eror,data) => {
+  console.log(data);
+  console.log(eror)
+})
